4b8f83298f126f6db55da80d31b930a8f8c47b25
V J Renuka <150992606+Runa-Rameena@users.noreply.github.com>
2026-03-20 22:57:00 +0530

    Fix data source for dataframe in streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,4 +41,4 @@ url = f"https://my.smoothiefroot.com/api/fruit/{fruit}"
 
 smoothiefroot_response = requests.get(url).json()
 #st.text(smoothiefroot_response)
-sf_df= st.dataframe(data =smoothiefroot_response.json(),use_container_width=True)
+sf_df= st.dataframe(data =smoothiefroot_response,use_container_width=True)
